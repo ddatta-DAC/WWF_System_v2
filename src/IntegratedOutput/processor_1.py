1010ee86e78246f241553b58131631e8c371d00f
ddatta-DAC <ddatta@vt.edu>
2019-10-16 17:59:51 -0400

    update

--- src/IntegratedOutput/processor_1.py
+++ src/IntegratedOutput/processor_1.py
@@ -9,7 +9,10 @@ import sys
 import argparse
 import math
 from sklearn import preprocessing
+
 CONFIG_FILE = './config.yaml'
+
+
 # ----
 
 def get_cur_path():
@@ -31,12 +34,12 @@ def set_up_config(
         CONFIG = yaml.safe_load(f)
 
     DIR = dir
-    MODEL_RESULTS_DIR =  CONFIG['MODEL_RESULTS_LOC']
+    MODEL_RESULTS_DIR = CONFIG['MODEL_RESULTS_LOC']
     SUBDIRS = []
-    for f in sorted( glob.glob( MODEL_RESULTS_DIR + '/**')) :
-        SUBDIRS.append( f.split('/')[-1])
+    for f in sorted(glob.glob(MODEL_RESULTS_DIR + '/**')):
+        SUBDIRS.append(f.split('/')[-1])
 
-    return CONFIG ,MODEL_RESULTS_DIR, SUBDIRS
+    return CONFIG, MODEL_RESULTS_DIR, SUBDIRS
 
 
 # Apply HS Code based filtering
@@ -44,23 +47,24 @@ def set_up_config(
 '''
 Collate the scored records from model output
 '''
-def collate_scored_records (CONFIG, DATA_DIR , DIR):
 
-    files = sorted(glob.glob(DATA_DIR+ '/' + DIR + '/**/**.csv'))
+
+def collate_scored_records(CONFIG, DATA_DIR, DIR):
+    files = sorted(glob.glob(DATA_DIR + '/' + DIR + '/**/**.csv'))
     print(files)
     master_df = None
-    for f in files :
+    for f in files:
         df = pd.read_csv(
-            f, index_col=None , low_memory=False
+            f, index_col=None, low_memory=False
         )
         # Scale the results between 0 and 1 for each
-        x = np.reshape(df['score'].values,[-1,1])
+        x = np.reshape(df['score'].values, [-1, 1])
         min_max_scaler = preprocessing.MinMaxScaler()
         x_scaled = min_max_scaler.fit_transform(x)
-        df['score'] = np.reshape(x_scaled,[-1])
+        df['score'] = np.reshape(x_scaled, [-1])
 
         if master_df is not None:
-            master_df =  master_df.append(df,ignore_index=True)
+            master_df = master_df.append(df, ignore_index=True)
         else:
             master_df = df
 
@@ -73,12 +77,14 @@ def collate_scored_records (CONFIG, DATA_DIR , DIR):
     master_df.to_csv(op_f_path)
     return master_df
 
+
 def get_HDF_df(CONFIG, DIR):
-    _path = os.path.join( CONFIG['HDF_OUTPUT_LOC'], DIR)
+    _path = os.path.join(CONFIG['HDF_OUTPUT_LOC'], DIR)
     f_name = 'HDF_results.csv'
-    f_path = os.path.join(_path,f_name)
-    df = pd.read_csv(f_path,index_col=None, low_memory=False)
+    f_path = os.path.join(_path, f_name)
+    df = pd.read_csv(f_path, index_col=None, low_memory=False)
     return df
+
 
 def combine(model_op_df, hdf_op_df):
     id_col = 'PanjivaRecordID'
@@ -86,41 +92,44 @@ def combine(model_op_df, hdf_op_df):
     hdf_df = pd.DataFrame(
         hdf_op_df.loc[hdf_op_df[id_col].isin(valid_ids)]
     )
-    combined_df = model_op_df.merge(hdf_df, how='inner', on =id_col)
+    combined_df = model_op_df.merge(hdf_df, how='inner', on=id_col)
 
     # ==========
     # Set up combined hdf score first
     # ==========
 
     def get_combined_hdf_score(row):
-        _list = ['LEB_flag', 'CITES_flag', 'WWF_HighRisk_flag', 'IUCN_RedList_flag','Lacey_Act_flag']
+        _list = ['LEB_flag', 'CITES_flag', 'WWF_HighRisk_flag', 'IUCN_RedList_flag', 'Lacey_Act_flag']
         _sum = 0
         for c in _list:
             _sum += row[c]
         return _sum
 
-    def calculate_combined_score(row,_max):
+    def calculate_combined_score(row, _max):
         s1 = 1 - row['score']
         s2 = row['hdf_score']
-        return s1* math.exp(s2/_max)
+        return s1 * math.exp(s2 / _max)
 
     combined_df['hdf_score'] = 0
     combined_df['hdf_score'] = combined_df.apply(
-        get_combined_hdf_score, axis = 1
+        get_combined_hdf_score, axis=1
     )
 
     combined_df['combined_score'] = 0
     _max = max(list(combined_df['hdf_score']))
-    _max = max(_max,1)
+    _max = max(_max, 1)
     combined_df['combined_score'] = combined_df.apply(
-        calculate_combined_score, axis= 1, args=(_max,)
+        calculate_combined_score, axis=1, args=(_max,)
     )
-    return  combined_df
+    return combined_df
+
+
+
 
 def main(dir):
     DIR = dir
-    CONFIG , MODEL_RESULTS_DIR, SUBDIRS = set_up_config(DIR)
-    model_op_df =  collate_scored_records (CONFIG, MODEL_RESULTS_DIR, DIR)
+    CONFIG, MODEL_RESULTS_DIR, SUBDIRS = set_up_config(DIR)
+    model_op_df = collate_scored_records(CONFIG, MODEL_RESULTS_DIR, DIR)
     hdf_op_df = get_HDF_df(CONFIG, DIR)
     # ================
     # Combine the model output and HDF output
@@ -129,7 +138,7 @@ def main(dir):
     final_df = combine(model_op_df, hdf_op_df)
     OP_LOC = CONFIG['MODEL_RESULTS_LOC']
     f_name = CONFIG['COMBINED_OP_FILE_NAME']
-    f_path = os.path.join(OP_LOC,DIR, f_name)
+    f_path = os.path.join(OP_LOC, DIR, f_name)
     final_df.to_csv(
         f_path,
         index=None
@@ -142,13 +151,13 @@ parser.add_argument(
     nargs='?',
     type=str,
     help=' < Data source > ',
-    choices=['us_import', 'china_import', 'china_export','peru_export']
+    choices=['us_import', 'china_import', 'china_export', 'peru_export']
 )
 
 args = parser.parse_args()
 
-if args.dir is not None :
+if args.dir is not None:
     print('Calling main :: ', args.dir)
     main(
-        dir = args.dir
+        dir=args.dir
     )
